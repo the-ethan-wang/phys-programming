757aad7e44db38ca6db53c7b148016fc29d8721f
the-ethan-wang <ethanwang314159@gmail.com>
2026-04-24 19:14:56 +1000

    blocks strings pulleys part 1

---
+++ blocks_strings_pulleys/README.md
@@ -0,0 +1,57 @@
+# blocks, strings, and pulleys
+
+Simple block, strings, and pulley systems  
+
+STILL WIP!!!! Just implemented block systems.  
+Tested against my solutions, they work for the questions in the worksheet.
+
+## Sample output
+```
+-------------------Sample block-------------------
+Net acceleration of system: 10.00ms^-2
+Net force of system: 10.00N
+Forces on each block:
+Block 1 | 1.00Kg | Applied: 10.00N, Contact Reaction: 0.00N
+
+
+--------------------Question 1--------------------
+Net acceleration of system: 6.00ms^-2
+Net force of system: 48.00N
+Forces on each block:
+Block 1 | 3.00Kg | Applied: 48.00N, Contact Reaction: -30.00N
+Block 2 | 5.00Kg | Contact: 30.00N
+
+
+--------------------Question 2--------------------
+Net acceleration of system: -6.00ms^-2
+Net force of system: -48.00N
+Forces on each block:
+Block 1 | 3.00Kg | Applied: -48.00N, Contact Reaction: 30.00N
+Block 2 | 5.00Kg | Contact: -30.00N
+
+
+--------------------Question 3--------------------
+Net acceleration of system: 8.00ms^-2
+Net force of system: 112.00N
+Forces on each block:
+Block 1 | 2.00Kg | Applied: 112.00N, Contact Reaction: -96.00N
+Block 2 | 4.00Kg | Contact: 96.00N, Contact Reaction: -64.00N
+Block 3 | 8.00Kg | Contact: 64.00N
+
+
+--------------------Question 4--------------------
+Net acceleration of system: -3.00ms^-2
+Net force of system: -33.00N
+Forces on each block:
+Block 1 | 7.00Kg | Applied: -33.00N, Contact Reaction: 12.00N
+Block 2 | 4.00Kg | Contact: -12.00N
+
+
+--------------------Question 5--------------------
+Net acceleration of system: 5.00ms^-2
+Net force of system: 40.00N
+Forces on each block:
+Block 1 | 3.00Kg | Applied: 40.00N, Contact Reaction: -25.00N
+Block 2 | 5.00Kg | Contact: 25.00N
+--------------------------------------------------
+```
---
+++ blocks_strings_pulleys/main.py
@@ -0,0 +1,71 @@
+from typing import Optional, Union
+
+def blocks(weights: list[Union[float,int]], net_force: Optional[Union[float,int]], net_acceleration: Optional[Union[float,int]]):
+    """Returns list of every force acting on each object. Assumes force is applied to the side of the weights."""
+    for weight in weights:
+        if weight<0:
+            print("Weights must be non-negative.")
+            return
+        
+    if len(weights)<1:
+        print("Must include at least 1 block.")
+        return
+
+    total_mass=sum(weights)
+    if net_acceleration and net_force:
+        if net_force!=net_acceleration*total_mass:
+            print("Net force conflicts with net acceleration")
+            return
+    elif net_acceleration:
+        net_force = net_acceleration*total_mass
+    elif net_force:
+        net_acceleration = net_force/total_mass
+    else:
+        print("Not enough information")
+        return
+    
+    ans={"forces": [], 'net acceleration': net_acceleration, 'net force': net_force}
+    for i, weight in enumerate(weights):
+        obj_net_force = net_acceleration*weight
+        obj_data={"name": f"Block {i+1}", "weight": weight, "forces": [], "net force": obj_net_force}
+        if i==0:
+            obj_data["forces"].append(["Applied", net_force])
+            obj_data["forces"].append(["Contact Reaction", obj_net_force-net_force])
+        elif i==len(weights)-1:
+            obj_data["forces"].append(["Contact", obj_net_force])
+        else:
+            obj_data["forces"].append(["Contact", -1*ans['forces'][i-1]['forces'][-1][1]])
+            obj_data["forces"].append(["Contact Reaction", obj_net_force-obj_data['forces'][0][1]])
+        ans['forces'].append(obj_data)
+    return ans
+
+examples=[
+    {"name": "Sample block", "weights": [1], "net_force": 10},
+    {"name": "Question 1", "weights": [3, 5], "net_force": 48},
+    {"name": "Question 2", "weights": [3, 5], "net_force": -48},
+    {"name": "Question 3", "weights": [2, 4, 8], "net_force": 112},
+    {"name": "Question 4", "weights": [7, 4], "net_acceleration": -3},
+    {"name": "Question 5", "weights": [3, 5], "net_acceleration": 5}
+]
+
+for example in examples:
+    weights=example["weights"]
+    net_force=example.get("net_force")
+    net_acceleration=example.get("net_acceleration")
+    data=blocks(weights=weights, net_force=net_force, net_acceleration=net_acceleration)
+    
+    assert data
+    padding_count=50-len(example["name"])
+    print("\n")
+    print(str(example["name"]).center(50, "-"))
+    print("Net acceleration of system:", f"{data['net acceleration']:.2f}ms^-2")
+    print("Net force of system:", f"{data['net force']:.2f}N")
+    print("Forces on each block:")
+    for block in data['forces']:
+        print(block["name"], "|", f"{block['weight']:.2f}Kg", "| ",end="")
+        print(", ".join([f"{x[0]}: {x[1]:.2f}N" for x in block["forces"]]))
+
+print("-"*50)
+
+
+# haha line 67
